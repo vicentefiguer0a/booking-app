5d48fe202fd5f88a9aea09040ebd20cfae669e69
Vicente Figueroa <figueroa.vicente9@gmail.com>
2023-03-03 01:12:36 -0800

    added: endpoint security

--- booking-server/src/main/java/com/example/bookingserver/security/WebSecurityConfig.java
+++ booking-server/src/main/java/com/example/bookingserver/security/WebSecurityConfig.java
@@ -1,17 +1,31 @@
 package com.example.bookingserver.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
 
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public WebSecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().disable();
-        http.authorizeHttpRequests().anyRequest().permitAll();
+        // Running instance of JwtRequestFilter before AuthorizationFilter is run.
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+        http.authorizeHttpRequests()
+                .requestMatchers("/register").permitAll()
+                .requestMatchers("/auth/login").permitAll()
+                .anyRequest().authenticated();
         return http.build();
     }
 }
